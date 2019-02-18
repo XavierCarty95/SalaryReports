625a1be2b6e05c0f420badab9d0c2d06d0fce8e4
Xavier Carty <xaviercarty.student@careerdevs.com>
2019-02-18 21:56:34 +0000

    seperating the data

--- salaryreport.js
+++ salaryreport.js
@@ -18,56 +18,22 @@ for (var i =0; i < 10; i++){
 }
 console.log(open)
 
-var newdata =   [ "10001","d005","1986-06-26","9999-01-01",  
-                "10002","d007","1996-08-03","9999-01-01", 
-                "10003","d004","1995-12-03","9999-01-01", 
-                "10004","d004","1986-12-01","9999-01-01", 
-                "10005","d003","1989-09-12","9999-01-01", 
-                "10006","d005","1990-08-05","9999-01-01", 
-                "10007","d008","1989-02-10","9999-01-01", 
-                "10008","d005","1998-03-11","2000-07-31", 
-                "10009","d006","1985-02-18","9999-01-01", 
-                "10010","d004","1996-11-24","2000-06-26" ]
-                
-newdata.pop([2])
-console.log(newdata)
-var arrays = [];
+var text2 = fs.readFileSync("load_salaries1.txt", "utf8")
+var data2 = text2.split('\n')
+// console.log(data2)
+var open2 = ""
+for(var i = 0; i < 120; i++){
+    open2 += "[" + data2[i]+  " ] " + "\n"
+}
+console.log(open2)
 
-for (var i = 0; i < newdata.length; i++){
-    if(i === "d005"){
-        arrays.concat(newdata)
-        console.log(arrays)
-    }
+var text3 = fs.readFileSync("load_employee.txt", "utf8")
+var data3 = text3.split('\n')
+
+var open3 = ""
+for(var i = 0; i < 10; i++){
+    open3 += "[" + data3[i]+  " ] " + "\n"
+    
 }
 
- console.log(arrays)
-//  for (var i = 0; i < 10;  i++){
-//      var len = textByLine[i
-//     console.log(len)
-// }
-
-// var open = []
-// for(var j = 0; j < len[i].length; j++) {
-//   var add = open.concat(len[i])
-//   console.log(add)
-// }
-    
- 
-
-   
-//  console.log(textByLine)
- 
-
-    
-
-// fs.readFile("load_employee.txt", "utf-8", function(err,data){
-//     if (err) throw err;
-//     var array = data.split("\n")
-//     console.log(data)
-    
-    
-// })
-
-// })
-
-
+console.log(open3)
