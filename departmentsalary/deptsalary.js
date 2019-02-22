5c782cfed4ebea3a4c7147cefd59d7555972215e
Xavier Carty <xaviercarty.student@careerdevs.com>
2019-02-22 00:55:24 +0000

    single dimensional arrays

---
+++ departmentsalary/deptsalary.js
@@ -0,0 +1,35 @@
+var fs = require('fs');
+
+// Step 1 Create all single-d and multi-d arrays AS empty arrays (initially) 
+    // push single string dat/elements into array as a single element 
+    // push array data into an array to form muilt-d arrays 
+    
+// single-d arrays 
+var departmentId = [];
+var departments = [];
+
+// multi -d arrays -- currently invalid
+var employeeId = [[], [], [],];
+var employeeName = [[], [], [],];
+var salaries = [];
+    
+// Process 'load_dept_name.txt' file
+
+fs.readFile( 'load_dept_names.txt', 'utf8', function(err,data){
+    if (err) throw err
+    var deptDataClean = data.replace(/INSERT INTO `departments` VALUES \n/g, "")
+    var deptDataArray = deptDataClean.split('\n')
+    for (var i = 0; i < deptDataArray.length; i++){
+        
+        //populate single-d arrays with DATA
+        departmentId.push(deptDataArray[i].slice(2,6));
+        departments.push(deptDataArray[i].slice(9,-3));
+        
+        // populate multi-d arrays with empty sub-arrays (NO DATA!!!)
+    }
+      
+         console.log(departmentId)
+         console.log(departments)
+    
+})
+
